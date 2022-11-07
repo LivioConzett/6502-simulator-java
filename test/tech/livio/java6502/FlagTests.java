a152460da192e44f2528c62b56e626cc585fae40
Livio Conzett (conzeliv) <conzeliv@students.zhaw.ch>
2022-11-07 19:29:03 +0100

    fixed Sonar Linter stuff

--- test/tech/livio/java6502/FlagTests.java
+++ test/tech/livio/java6502/FlagTests.java
@@ -1,16 +1,24 @@
 package tech.livio.java6502;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class FlagTests {
+
+    private Flags flags;
+
+    @BeforeEach
+    void init(){
+        this.flags = new Flags();
+    }
+
     @Test
-    void flagTests() {
-        Flags flags = new Flags();
+    void flagInitTests() {
 
         Assertions.assertTrue(flags.getInterruptDisable());
 
-        flags.setWholeRegister((byte)0xff);
+        flags.setWholeRegister((byte) 0xff);
         Assertions.assertTrue(flags.getCarry());
         Assertions.assertTrue(flags.getZero());
         Assertions.assertTrue(flags.getInterruptDisable());
@@ -18,6 +26,10 @@ class FlagTests {
         Assertions.assertTrue(flags.getBreakCommand());
         Assertions.assertTrue(flags.getOverFlow());
         Assertions.assertTrue(flags.getNegative());
+    }
+
+    @Test
+    void flagResetTest() {
 
         flags.reset();
         Assertions.assertFalse(flags.getCarry());
@@ -28,6 +40,11 @@ class FlagTests {
         Assertions.assertFalse(flags.getOverFlow());
         Assertions.assertFalse(flags.getNegative());
 
+    }
+
+    @Test
+    void flagSetTest() {
+
         flags.setCarry(true);
         flags.setZero(true);
         flags.setInterruptDisable(true);
@@ -37,7 +54,7 @@ class FlagTests {
         flags.setNegative(true);
 
         Assertions.assertTrue(flags.getCarry());
-        Assertions.assertEquals(1,flags.getCarryInt());
+        Assertions.assertEquals(1, flags.getCarryInt());
         Assertions.assertTrue(flags.getZero());
         Assertions.assertTrue(flags.getInterruptDisable());
         Assertions.assertTrue(flags.getDecimalMode());
@@ -54,13 +71,18 @@ class FlagTests {
         flags.setNegative(false);
 
         Assertions.assertFalse(flags.getCarry());
-        Assertions.assertEquals(0,flags.getCarryInt());
+        Assertions.assertEquals(0, flags.getCarryInt());
         Assertions.assertFalse(flags.getZero());
         Assertions.assertFalse(flags.getInterruptDisable());
         Assertions.assertFalse(flags.getDecimalMode());
         Assertions.assertFalse(flags.getBreakCommand());
         Assertions.assertFalse(flags.getOverFlow());
         Assertions.assertFalse(flags.getNegative());
+
+    }
+
+    @Test
+    void flagSetWholeTest(){
 
         flags.setWholeRegister((byte) 0xff);
 
