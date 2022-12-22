62567188e4cfa2d3de0e1fe09e343a5f05a9fcd0
Livio Conzett (conzeliv) <conzeliv@students.zhaw.ch>
2022-12-22 18:36:33 +0100

    created remove comment method and tests

--- src/tech/livio/java6502/Compiler.java
+++ src/tech/livio/java6502/Compiler.java
@@ -1,6 +1,9 @@
 package tech.livio.java6502;
 
-import java.util.Map;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * Compiles 6502 assembly code
@@ -9,6 +12,8 @@ public class Compiler {
 
     private CompilerCallBack statusChangeCallback;
     private CompilerStatus status;
+    private List<Byte> opList;
+    private final String regComment = " *;.*";
 
     public Compiler(){
         this.statusChangeCallback = c -> {
@@ -17,10 +22,12 @@ public class Compiler {
                 System.out.println(c.errorType().toString());
                 System.out.println("line: "+c.getLine());
             }
-            System.out.println(c.details());
+            System.out.print(c.details());
         };
 
-        this.status = new CompilerStatus("Initialization of Compiler");
+        this.status = new CompilerStatus("Initialization of Compiler\n");
+        this.opList = new ArrayList<>();
+
     }
 
     /**
@@ -40,4 +47,66 @@ public class Compiler {
         this.status = compilerStatus;
         this.statusChangeCallback.run(compilerStatus);
     }
+
+    /**
+     * Resets the compiler
+     */
+    private void reset(){
+        this.opList.clear();
+
+    }
+
+    /**
+     * Turns the opList into a byteArray
+     * @return Array of bytes
+     */
+    private byte[] listToArray(){
+        int length = this.opList.size();
+        byte[] opArray = new byte[length];
+
+        for(int i = 0; i < length; i++){
+            opArray[i] = this.opList.get(i);
+        }
+        return opArray;
+    }
+
+    /**
+     * Takes a string and compiles it.
+     * @param code 6502 assembly code to compile
+     */
+    byte[] compile(String code){
+        this.reset();
+
+        changeStatus(new CompilerStatus("-- Compiler starting --"));
+
+        String[] codeArray = code.split("\s");
+
+        // remove the comments from the code
+        String[] noComments = removeComments(codeArray);
+
+
+
+
+
+        return this.listToArray();
+    }
+
+    /**
+     * Remove the comments from the code
+     * @param code code to remove the comments from
+     * @return code without comments
+     */
+    String[] removeComments(String[] code){
+
+        changeStatus(new CompilerStatus("Removing comments..."));
+
+        for(int lineNumber = 0; lineNumber < code.length; lineNumber++){
+            code[lineNumber] = code[lineNumber].replaceAll(regComment,"");
+        }
+
+        changeStatus(new CompilerStatus("done.\n"));
+
+        return code;
+    }
+
 }
--- test/tech/livio/java6502/CompilerTest.java
+++ test/tech/livio/java6502/CompilerTest.java
@@ -1,11 +1,33 @@
 package tech.livio.java6502;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CompilerTest {
 
     @Test
-    void statusChange(){
+    void removeComments(){
+
+        Compiler compiler = new Compiler();
+
+        String input[] = {
+                "  and ;this is a comment",
+                "  ;hello",
+                ";what are you doing",
+                "nop   ;yo",
+                "hello the     "
+        };
+
+        String output[] = {
+                "  and",
+                "",
+                "",
+                "nop",
+                "hello the     "
+
+        };
+
+        Assertions.assertArrayEquals(output, compiler.removeComments(input));
 
     }
 
