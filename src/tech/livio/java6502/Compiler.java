8b54fa893631d56e8cce95bf7a8015b9336e66a5
Livio Conzett <5398351+LivioConzett@users.noreply.github.com>
2023-03-02 08:47:26 +0100

    added the BSD 3-clause license

--- src/tech/livio/java6502/Compiler.java
+++ src/tech/livio/java6502/Compiler.java
@@ -1,5 +1,12 @@
-package tech.livio.java6502;
+/*
+Copyright (c) 2023, Livio Conzett
+All rights reserved.
 
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+package tech.livio.java6502;
 
 import java.util.HashMap;
 import java.util.Map;
